d2b0968477e3bab3b98b6e847a3c1335c68151c3
taslimaakterpapia <taslimaakterpapia495@gmail.comm>
2023-05-04 23:00:25 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+var cors = require('cors');
+const port = process.env.PORT || 5000;
+const recipe = require("./data/data.json")
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World! ')
+});
+app.get('/recipes', (req, res)=> {
+    res.send(recipe);
+})
+
+app.get('/recipes/:id',(req,res)=>{
+    const id=req.params.id;
+    console.log(id)
+    const selectedRecipes=recipe.find(res=>res.id==id)
+    res.send(selectedRecipes);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
