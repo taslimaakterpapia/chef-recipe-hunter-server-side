98cc16797fab32a80fb4a75a59766b47e8e0ba70
taslimaakterpapia <taslimaakterpapia495@gmail.comm>
2023-05-05 20:51:05 +0600

    done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 var cors = require('cors');
 const port = process.env.PORT || 5000;
-const recipe = require("./data/data.json")
+const recipe = require("./Data/data.json")
 app.use(cors())
 
 app.get('/', (req, res) => {
